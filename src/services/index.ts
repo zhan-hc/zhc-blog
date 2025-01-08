ce9058346ea711a9b65369d5ce89d1a5c751b8f4
hc-zhan <huanchao_zhan@feidee.com>
2025-01-08 16:27:29 +0800

    fix: add timeout

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,7 @@ import { ENV, envApiHost, getNodeEnv } from '@/utils/env'
 import MyAxios from './axios'
 const MyRequest =  new MyAxios({
   baseURL: getNodeEnv === ENV.DEV ? '/blogApi' : envApiHost,
-  timeout: 3000
+  timeout: 10000
 })
 
 export default MyRequest
